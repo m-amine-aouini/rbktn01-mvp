d810830f0fc4bcfce5aed1ca9c9b20485425af8a
aminaouini <aminaouini565@gmail.com>
2020-04-13 03:50:31 +0100

    deploying to heroku

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ require('./routes/accRoutes')(app);
 require('./routes/productRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
 
+  app.use(express.static('client/build'));
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
